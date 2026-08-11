80daff73a96d51d72fd3c34b075cd54d311b92d7
Praharsha5969 <praharsha.palivela.123@gmail.com>
2026-08-11 20:28:15 +0530

    Time: 15 ms (60.42%) | Memory: 12.5 MB (17.68%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        i = 0
+        output = ""
+        while i < len(command):
+            if command[i] == 'G':
+                output += 'G'
+                i += 1
+            elif command[i] == '(':
+                if command[i+1] == ')':
+                    output +='o'
+                    i += 2
+                else :
+                    output += 'al'
+                    i+=4
+        return output
+                
+            
+        
