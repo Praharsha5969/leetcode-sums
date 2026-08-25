4132baadfe93f074b7d62e264cc9465be32e62af
Praharsha5969 <praharsha.palivela.123@gmail.com>
2026-08-25 21:45:09 +0530

    Time: 39 ms (13.73%) | Memory: 37.5 MB (7.56%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        s = set(nums)
-        return len(s) != len(nums)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num,0) + 1
+            if freq[num] >= 2:
+                return True
+        return False
         
